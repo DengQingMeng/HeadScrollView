75ce93d553fa2509e86f479be73cc40c1942af09
AndyDeng <630338655@qq.com>
2018-06-21 10:40:47 +0800

    初始化工程

---
+++ HeadScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeadScrollView
+//
+//  Created by AndyDeng on 2018/6/21.
+//  Copyright © 2018年 AndyDeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
